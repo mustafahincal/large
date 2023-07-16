b711a3b9c91e6e50ec2d0777fa1625d6fc88705c
mustafahincal <mustafahncal@gmail.com>
2023-07-16 21:07:24 +0300

    up: api docs

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,40 +1,23 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
 
-/* const userData: Prisma.UserCreateInput[] = [
-  {
-    name: "Alice",
-    email: "alice@prisma.io",
-  },
-  {
-    name: "Nilu",
-    email: "nilu@prisma.io",
-  },
-  {
-    name: "Mahmoud",
-    email: "mahmoud@prisma.io",
-  },
-]; */
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Veritabanına bağlandı");
 
-/* async function main() {
-  console.log(`Start seeding ...`);
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    });
-    console.log(`Created user with id: ${user.id}`);
+    await prisma.$disconnect();
+    console.log("Veritabanı bağlantısı kesildi");
+  } catch (error) {
+    console.error("Bir hata oluştu:", error);
   }
-  console.log(`Seeding finished.`);
 }
 
 main()
-  .then(async () => {
+  .catch((error) => console.error("Ana işlev hatası:", error))
+  .finally(async () => {
     await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
   });
- */
