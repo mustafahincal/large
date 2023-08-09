c0ffc6721b3e8746a99fd782bc2f4f372ad1afcd
mustafahincal <mustafahncal@gmail.com>
2023-08-09 22:12:56 +0300

    up: user model add role field

--- v1/src/controllers/Auth.ts
+++ v1/src/controllers/Auth.ts
@@ -23,12 +23,14 @@ class AuthController {
             first_name: user.first_name,
             last_name: user.last_name,
             email: user.email,
+            ROLE: user.role,
           }),
           refresh_token: generateRefreshToken({
             id: user.id.toString(),
             first_name: user.first_name,
             last_name: user.last_name,
             email: user.email,
+            ROLE: user.role,
           }),
         },
       });
--- v1/src/controllers/User.ts
+++ v1/src/controllers/User.ts
@@ -1,23 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import userService from "../services/User";
-import { Prisma, User } from "@prisma/client";
 import httpStatus from "http-status";
 import Messages from "../constants/Messages";
 import { CustomError } from "../utils/customError";
+import { User } from "@prisma/client";
+
+type Key = keyof User;
 
 class UsersController {
-  /*   exclude<User, Key extends keyof User>(
-    user: User,
-    keys: Key[]
-  ): Omit<User, Key> {
-    return Object.fromEntries(
-      Object.entries(user).filter(([key]) => !keys.includes(key))
-    );
-  } */
+  static exclude(user: User, ...keys: Key[]) {
+    for (let key of keys) {
+      delete user[key];
+    }
+    return user;
+  }
 
   async index(req: Request, res: Response, next: NextFunction) {
     try {
       const users = await userService.list();
+      users.map((user) => UsersController.exclude(user, "password"));
       res.status(httpStatus.OK).send({
         status: httpStatus.OK,
         message: "List of Users",
@@ -32,10 +33,11 @@ class UsersController {
     try {
       const { id } = req.params;
       const user = await userService.get({ id });
+      if (!user) throw new CustomError(404, Messages.UserNotFound);
       res.status(httpStatus.OK).send({
         status: httpStatus.OK,
-        message: "User",
-        data: user,
+        message: "Users listed",
+        data: UsersController.exclude(user, "password"),
       });
     } catch (err) {
       next(err);
