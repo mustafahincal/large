35574bc0a52fdd2801262d79ccd8b1feffea0975
mustafahincal <mustafahncal@gmail.com>
2023-10-26 15:29:56 +0300

    fix: remote git address

--- v1/src/routes/comment.ts
+++ v1/src/routes/comment.ts
@@ -1,17 +1,19 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import commentsController from "../controllers/Comments";
-import { errorCatcher } from "../utils/errorCatcher";
+import commentsController from '../controllers/Comments';
+import { errorCatcher } from '../utils/errorCatcher';
 
 router
-  .route("/")
+  .route('/')
   .get(errorCatcher(commentsController.index))
   .post(errorCatcher(commentsController.add));
 router
-  .route("/:id")
+  .route('/:id')
   .get(errorCatcher(commentsController.getById))
   .delete(errorCatcher(commentsController.remove))
   .patch(errorCatcher(commentsController.patch));
-router.route("/blog/:blogId").get(errorCatcher(commentsController.getByBlog));
+router.route('/blog/:blogId').get(errorCatcher(commentsController.getByBlog));
+
+// deneme degisiklik
 
 export default router;
