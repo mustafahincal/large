110bc41d2f1deef047480b509fc17394ccb2ae21
mustafahincal <mustafahncal@gmail.com>
2023-10-26 15:31:04 +0300

    del: comment line

--- v1/src/routes/comment.ts
+++ v1/src/routes/comment.ts
@@ -14,6 +14,4 @@ router
   .patch(errorCatcher(commentsController.patch));
 router.route('/blog/:blogId').get(errorCatcher(commentsController.getByBlog));
 
-// deneme degisiklik
-
 export default router;
